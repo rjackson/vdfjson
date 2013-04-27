612272f7eaff6ca4e0ddfd9d21e256254019635c
RJackson <robjacksonm1@gmail.com>
2013-04-27 11:15:45 +0100

    Initial commit.

---
+++ index.py
@@ -0,0 +1,80 @@
+import json
+import os
+# import uwsgi
+
+import vdf
+import web
+#web.config.debug = False
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+urls = ("/vdfjson/img/(.*)", 'images',
+        "/vdfjson/css/(.*)", 'css',
+        "/vdfjson/js/(.*)", 'js',
+        "/vdfjson/", 'index')
+
+render = web.template.render('templates')
+
+
+class index:
+    def GET(self):
+        return render.index()
+
+    def POST(self):
+        _input = web.input()
+        format = _input.get("format")
+
+        if format == "vdf":
+            data = json.dumps(
+                vdf.loads(_input.get("data")),
+                indent=4
+            )
+
+        if format == "json":
+            _data = json.loads(_input.get("data"))
+            data = vdf.dumps(_data).decode("utf-16")
+
+        return render.formatted(data, format)
+
+
+class images:
+    def GET(self, name):
+        ext = name.split(".")[-1]
+
+        cType = {
+            "png": "image/png",
+            "jpg": "image/jpeg",
+            "gif": "image/gif",
+            "ico": "image/x-icon"
+        }
+
+        if name in os.listdir(os.path.join(path, 'images')):
+            web.header("Content-Type", cType[ext])
+            return open(os.path.join(path, 'images', '{}'.format(name)), "rb").read()
+        else:
+            raise web.notfound()
+
+
+class css:
+    def GET(self, name):
+        if name in os.listdir(os.path.join(path, 'css')):
+            web.header("Content-Type", "text/css")
+            return open(os.path.join(path, 'css', '{}'.format(name)), "r").read()
+        else:
+            raise web.notfound()
+
+
+class js:
+    def GET(self, name):
+        if name in os.listdir(os.path.join(path, 'js')):
+            web.header("Content-Type", "application/js")
+            return open(os.path.join(path, 'js', '{}'.format(name)), "r").read()
+        else:
+            raise web.notfound()
+
+
+app = web.application(urls, globals())
+if __name__ == "__main__":
+    app.run()
+
+application = app.wsgifunc()
